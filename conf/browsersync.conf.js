7527bc2f21649a932d1d53363613bd7c808e7306
Vasiliy Malakhov <mvamgc@gmail.com>
2017-03-14 20:40:09 -0400

    proxy to SockJS

--- conf/browsersync.conf.js
+++ conf/browsersync.conf.js
@@ -1,4 +1,12 @@
 const conf = require('./gulp.conf');
+const proxy = require('http-proxy-middleware');
+
+const midiSyncProxy = proxy('/api/midi', {
+  target: 'http://localhost:3010',
+  changeOrigin: true,
+  ws: true,
+  logLevel: 'debug'
+});
 
 module.exports = function () {
   return {
@@ -6,7 +14,8 @@ module.exports = function () {
       baseDir: [
         conf.paths.tmp,
         conf.paths.src
-      ]
+      ],
+      middleware: [midiSyncProxy]
     },
     open: false
   };
