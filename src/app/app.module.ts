1ddf501c8db2e11c516c0df02e7d522ffc8d70c8
Vasiliy Malakhov <mvamgc@gmail.com>
2017-03-15 19:44:38 -0400

    Fixing bootsrap: dist and enabling ui.bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,6 @@ import {module} from 'angular';
 
 import routesConfig from '../routes';
 
-export let app = module('app', ['ui.router']).config(routesConfig);
+export let app = module('app', ['ui.router', 'ui.bootstrap']).config(routesConfig);
 
 import './main.component';
