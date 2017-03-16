8609eeca0f2225cba4b113289a27d714f44a1a5f
Vasiliy Malakhov <mvamgc@gmail.com>
2017-03-15 22:10:59 -0400

    input and output selection

--- src/app/main.component.ts
+++ src/app/main.component.ts
@@ -1,18 +1,59 @@
 import {app} from './app.module';
 import {MidiService} from './midi.service';
 
+class MainController {
+  inputs: any[] = [];
+  selectedInput: any = {name: 'MIDI Input is not available'};
+  inputDisabled: boolean = true;
+
+  outputs: any[] = [];
+  selectedOutput: any = {name: 'MIDI Output is not available'};
+  outputDisabled: boolean = true;
+
+  /* @ngInject */
+  constructor(private $log: angular.ILogService, midiService: MidiService) {
+    midiService.connect().then(() => {
+      console.log('Output[0]: %o', midiService.getOutputs()[0]);
+      this.inputs = midiService.getInputs();
+      if(this.inputs.length > 0) {
+        this.inputSelection(this.inputs[this.inputs.length - 1].id);
+        // this.selectedInput = this.inputs[this.inputs.length - 1];
+        // this.inputDisabled = false;
+      }
+      this.outputs = midiService.getOutputs();
+      if(this.outputs.length > 0) {
+        this.outputSelection(this.outputs[this.outputs.length - 1].id);
+        // this.selectedOutput = this.outputs[this.outputs.length - 1];
+        // this.outputDisabled = false;
+      }
+      console.log(this.inputs);
+    });
+  }
+
+  inputSelection(inputId) {
+    this.$log.log('input selection: %o', inputId);
+    var selectedInputArray = this.inputs.filter(input => input.id === inputId);
+    if (selectedInputArray.length > 0) {
+      this.selectedInput = selectedInputArray[0];
+      this.inputDisabled = false;
+    }
+  }
+  outputSelection(outputId) {
+    this.$log.log('output selection: %o', outputId);
+    var selectedOutputArray = this.outputs.filter(output => output.id === outputId);
+    if (selectedOutputArray.length > 0) {
+      this.selectedOutput = selectedOutputArray[0];
+      this.outputDisabled = false;
+    }
+  }
+}
+
 export const main: angular.IComponentOptions = {
   template: require('./main.html'),
   /* @ngInject */
-  controller: function (midiService: MidiService) { // eslint-disable-line babel/object-shorthand
-    //
-    midiService.connect().then(() => {
-      console.log("Output[0]: %o", midiService.getOutputs()[0]);
-      this.inputs = midiService.getInputs();
-      console.log(this.inputs);
-    });
-
-  }
+  controller: MainController
 };
 
 app.component('main', main);
+
+
