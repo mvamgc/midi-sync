9135e6589a5650690b1a4d0d083686c570452faa
Vasiliy Malakhov <mvamgc@gmail.com>
2017-05-06 11:19:25 -0400

    #3 - Angular 2 migration - temporary returning AngularJS, routes and app.module.ts to fix build until end of migration

---
+++ src/app/app.module.ts
@@ -0,0 +1,11 @@
+import {module} from 'angular';
+
+import routesConfig from '../routes';
+
+export let app = module('app', ['ui.router', 'ui.bootstrap']).config(routesConfig);
+
+import './main.component';
+import './server-control.component';
+import './sockjs-midi.service';
+import './keyboard/midi-keys.component';
+import './keyboard/midi-keys-octave.component';
